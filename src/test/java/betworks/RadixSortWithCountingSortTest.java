539220052d43d696fc57de2c830a17143aa3dc32
Kosala <dukekosy@gmail.com>
2021-03-27 18:55:56 -0600

    test

--- src/test/java/betworks/RadixSortWithCountingSortTest.java
+++ src/test/java/betworks/RadixSortWithCountingSortTest.java
@@ -1,7 +1,22 @@
 package betworks;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 class RadixSortWithCountingSortTest {
 
+    private RadixSortWithCountingSort radixSortWithCountingSort;
+
+    @BeforeEach
+    public void setup() {
+         radixSortWithCountingSort = new RadixSortWithCountingSort(new int[]{});
+    }
+
+    @Test
+    public void getPrefixSums_successful() {
+        Assert.assertArrayEquals(new int[]{0,2,3,5,6,8,10,13,15,16},radixSortWithCountingSort.getPrefixSums(new int[]{0,2,1,2,1,2,2,3,2,1}));
+    }
 }
